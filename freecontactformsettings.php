8d354d09a86ac4a2444c53ba9f579e8f8bb2923e
pedrituss <pedrituss@gmail.com>
2014-11-16 02:12:03 +0100

    add hole website

---
+++ freecontactformsettings.php
@@ -0,0 +1,11 @@
+<?php
+
+$email_to = "info@trezzerock.es"; // your email address
+$email_subject = "TREZZE - Mensaje nuevo"; // email subject line
+$thankyou = "contacto2.html"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+$antispam_answer = "25";
+
+?>
